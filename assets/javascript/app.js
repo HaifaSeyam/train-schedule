cdd5fb9988d9837e3c1a7c2a5fa60021876ccbfb
HaifaSeyam <h.abuamro@hotmail.com>
2019-01-20 20:33:09 -0600

    displaying data on the screen/html document

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,12 +18,38 @@ var config = {
    var destination = $("#destination").val().trim();
    var firstTrainTime = $("#firstTrainTime").val().trim();
    var frequency = $("#frequency").val().trim();
-
         database.push({
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
             frequency: frequency
         });
+ });
+ 
+ database.on("child_added", function(snapshot){
 
+    var firstTT = moment(snapshot.val().firstTrainTime, "hhmm").format("hh:mm");
+    var tFrequency = snapshot.val().frequency;
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTT, "HH:mm").subtract(1, "years");
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
+
+    var tableRow = "<tr><td scope='col'>" + snapshot.val().trainName + "</td>" +
+                 "<td scope='col'>" + snapshot.val().destination + "</td>" +
+                 "<td scope='col'>" + snapshot.val().frequency + "</td>" +
+                 "<td scope='col'>" + nextTrain + "</td>" +
+                 "<td scope='col'>" + tMinutesTillTrain + "</td></tr>";
+
+    $("#tableBody").prepend(tableRow);
+
+ }, function (errorObject){
+    console.log("Errors handled:"+ errorObject.code)
  });
